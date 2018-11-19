78ca40e0113207d3e9d9c382f9ca2b9c14ca4e89
Ziqiang Tang <ziqiangt@163.com>
2018-11-18 18:48:29 -0800

    *

--- bcl/app.R
+++ bcl/app.R
@@ -1,47 +1,160 @@
 library(shiny)
-library(tidyverse)
+library(dplyr)
+library(ggplot2)
 
-bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
-# Define UI for application that draws a histogram
+
+# load the data (retrieve and clean raw data if this is the first time)
+filename <- file.path("data", "bcl-data.csv")
+if (file.exists(filename)) {
+  bcl <- read.csv(filename, stringsAsFactors = FALSE)
+} else {
+  bcl <- read.csv("http://pub.data.gov.bc.ca/datasets/176284/BC_Liquor_Store_Product_Price_List.csv",
+                  stringsAsFactors = FALSE)
+  products <- c("BEER", "REFRESHMENT BEVERAGE", "SPIRITS", "WINE")
+  bcl <- dplyr::filter(bcl, PRODUCT_CLASS_NAME %in% products) %>%
+    dplyr::select(-PRODUCT_TYPE_NAME, -PRODUCT_SKU_NO, -PRODUCT_BASE_UPC_NO,
+                  -PRODUCT_LITRES_PER_CONTAINER, -PRD_CONTAINER_PER_SELL_UNIT,
+                  -PRODUCT_SUB_CLASS_NAME) %>%
+    rename(Type = PRODUCT_CLASS_NAME,
+           Subtype = PRODUCT_MINOR_CLASS_NAME,
+           Name = PRODUCT_LONG_NAME,
+           Country = PRODUCT_COUNTRY_ORIGIN_NAME,
+           Alcohol_Content = PRODUCT_ALCOHOL_PERCENT,
+           Price = CURRENT_DISPLAY_PRICE,
+           Sweetness = SWEETNESS_CODE)
+  bcl$Type <- sub("^REFRESHMENT BEVERAGE$", "REFRESHMENT", bcl$Type)
+  dir.create("data", showWarnings = FALSE)
+  write.csv(bcl, filename, row.names = FALSE)
+}
+
+## UI
 ui <- fluidPage(
+  titlePanel("BC Liquor Store prices"),
   
-  ## Layouts: Together - panel
-  titlePanel("BC Liquor price app", 
-             windowTitle = "BCL app"),
+  #-------------------------------------------------------------------------------------
+  ## gif as logo
+  div(id="logo",
+      img(src = "logo.gif")
+  ),
+  #-------------------------------------------------------------------------------------
+  
   sidebarLayout(
-    sidebarPanel(sliderInput("priceInput", "Select your desired price range.",
-                             min = 0, max = 100, value = c(15, 30), pre="$"),
-                 
-                 radioButtons("typeInput", " Select one type of beverage.",
-                              choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                              selected = "WINE")),
+    sidebarPanel(
+      h4(
+        "Had a long day?  This app will help you find the right drink for tonight! Just use the filters below..."
+      ),
+      br(),
+      
+      #-------------------------------------------------------------------------------------
+      ## sort by price
+      checkboxInput("sortByPrice", "Sort by price", FALSE),
+      ## a conditionalPanel for ascending or descending ordering
+      conditionalPanel(
+        condition = "input.sortByPrice",
+        uiOutput("PriceSortOutput")),
+      #-------------------------------------------------------------------------------------
+      
+      sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
+      uiOutput("typeSelectOutput"),
+      checkboxInput("filterCountry", "Filter by country", FALSE),
+      conditionalPanel(
+        condition = "input.filterCountry",
+        uiOutput("countrySelectorOutput")
+      ),
+      hr(),
+      span("Data source:", 
+           tags$a("OpenDataBC",
+                  href = "https://www.opendatabc.ca/dataset/bc-liquor-store-product-price-list-current-prices")),
+      br(),
+      em(
+        span("Created by", a(href = "http://deanattali.com", "Dean Attali")),
+        HTML("&bull;"),
+        span("Code", a(href = "https://github.com/daattali/shiny-server/tree/master/bcl", "on GitHub")),
+        br(), 
+        span("Imprved code", a(href = "https://github.com/STAT545-UBC-students/hw08-ziqiangt", "Ziqiang's Git"))
+      )
+    ),
     mainPanel(
-      plotOutput("price_hist"),
-      tableOutput("price_table"))
+      h3(textOutput("summaryText")),
+      downloadButton("download", "Download results"),
+      br(),
+      plotOutput("plot"),
+      br(), br(),
+      #tableOutput("prices")
+      DT::dataTableOutput("prices")
+    )
   )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  observe(print(input$priceInput))
-   bcl_filtered <- reactive({
-     bcl %>% 
-      filter(Price < input$priceInput[2],
-             Price > input$priceInput[1],
-             Type == input$typeInput)
+server <- function(input, output, session) {
+  output$countrySelectorOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
   })
   
-   
-  output$price_hist <- renderPlot({
-    bcl_filtered() %>% 
-      ggplot(aes(Price)) +
-      geom_histogram()
+  output$typeSelectOutput <- renderUI({
+    selectInput("typeInput", "Product type",
+                sort(unique(bcl$Type)),
+                multiple = TRUE,
+                selected = c("BEER", "WINE"))
   })
-  output$price_table <- renderTable({
-    bcl_filtered()
+  
+  output$summaryText <- renderText({
+    numOptions <- nrow(prices())
+    if (is.null(numOptions)) {
+      numOptions <- 0
+    }
+    paste0("We found ", numOptions, " options for you")
   })
+  
+  prices <- reactive({
+    prices <- bcl
+    
+    if (is.null(input$countryInput)) {
+      return(NULL)
+    }
+    
+    prices <- dplyr::filter(prices, Type %in% input$typeInput)
+    if (input$filterCountry) {
+      prices <- dplyr::filter(prices, Country == input$countryInput)
+    }
+    
+    #-------------------------------------------------------------------------------------
+    if (input$sortByPrice){
+      prices <- dplyr::filter(prices, Price >= input$priceInput[1],
+                              Price <= input$priceInput[2])
+    }
+    #-------------------------------------------------------------------------------------
+    
+    if(nrow(prices) == 0) {
+      return(NULL)
+    }
+    prices
+  })
+  
+  output$plot <- renderPlot({
+    if (is.null(prices())) {
+      return(NULL)
+    }
+    
+    ggplot(prices(), aes(Alcohol_Content, fill = Type)) +
+      geom_histogram(colour = "black") +
+      theme_classic(20)
+  })
+  
+  output$prices <- DT::renderDataTable({
+    prices()
+  })
+  
+  output$download <- downloadHandler(
+    filename = function() {
+      "bcl-results.csv"
+    },
+    content = function(con) {
+      write.csv(prices(), con)
+    }
+  )
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
